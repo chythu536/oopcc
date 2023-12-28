3ad02cc408cbc6caa97515edf287faa2adce90bf
Kanegonda Ravi Chythanya <chythu536@users.noreply.github.com>
2023-12-28 10:10:27 +0000

    yes

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = in.next();
+        System.out.println("Hello, Mr. " + name);
+    }
+}
